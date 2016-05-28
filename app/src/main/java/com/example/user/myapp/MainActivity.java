14340499d2cc9d148a8923dd4da4afc43b7c58b1
jangjunje <jangjunje@gmail.com>
2016-05-28 17:02:31 +0900

    simple calculator : start

--- app/src/main/java/com/example/user/myapp/MainActivity.java
+++ app/src/main/java/com/example/user/myapp/MainActivity.java
@@ -10,24 +10,86 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    Button mbtnOk;
-    EditText metName;
-    TextView mtvName;
+    EditText meditNum1;
+    EditText meditNum2;
+    TextView mtextviewResult;
+    Button   mbtnPlus;
+    Button   mbtnMinus;
+    Button   mbtnMultiply;
+    Button   mbtnDivided;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("안녕하세요");
+        setTitle("Simple Calculator");
 
-        mbtnOk  = (Button)findViewById(R.id.btnOk);
-        metName = (EditText)findViewById(R.id.etName);
-        mtvName = (TextView)findViewById(R.id.tvName);
+        meditNum1    = (EditText)findViewById(R.id.num1);
+        meditNum2    = (EditText)findViewById(R.id.num2);
 
-        mbtnOk.setOnTouchListener(new View.OnTouchListener() {
+        mbtnPlus     = (Button)findViewById(R.id.btnPlus);
+        mbtnMinus    = (Button)findViewById(R.id.btnMinus);
+        mbtnMultiply = (Button)findViewById(R.id.btnMultiply);
+        mbtnDivided  = (Button)findViewById(R.id.btnDivided);
+
+        mtextviewResult  = (TextView) findViewById(R.id.textResult);
+
+        mbtnPlus.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                mtvName.setText("안녕하세요 : " + metName.getText().toString());
+
+                String Num1 = meditNum1.getText().toString();
+                String Num2 = meditNum2.getText().toString();
+
+                Integer result = Integer.parseInt(Num1) + Integer.parseInt(Num2);
+
+                mtextviewResult.setText("계산결과 " + result.toString());
+
+                return false;
+            }
+        });
+
+        mbtnMinus.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                String Num1 = meditNum1.getText().toString();
+                String Num2 = meditNum2.getText().toString();
+
+                Integer result = Integer.parseInt(Num1) - Integer.parseInt(Num2);
+
+                mtextviewResult.setText("계산결과 " + result.toString());
+
+                return false;
+            }
+        });
+
+        mbtnMultiply.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                String Num1 = meditNum1.getText().toString();
+                String Num2 = meditNum2.getText().toString();
+
+                Integer result = Integer.parseInt(Num1) * Integer.parseInt(Num2);
+
+                mtextviewResult.setText("계산결과 " + result.toString());
+
+                return false;
+            }
+        });
+
+        mbtnDivided.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                String Num1 = meditNum1.getText().toString();
+                String Num2 = meditNum2.getText().toString();
+
+                Integer result = Integer.parseInt(Num1) / Integer.parseInt(Num2);
+
+                mtextviewResult.setText("계산결과 " + result.toString());
+
                 return false;
             }
         });
